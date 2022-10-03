ff88e2f84c02de7b41dc555d29b3b09a12058f0a
Nikiforov Nikita <2naikn@gmail.com>
2022-10-03 16:09:28 +0300

    Create0310

---
+++ ResumeSite.php
@@ -0,0 +1,84 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=0, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta name="keywords" content="html,css,resume">
+<link rel="stylesheet" href="Styles/styles2.css">
+<title>Resume</title>
+</head>
+<body>
+    <header> 
+      <div class="logo">
+        <img src="Images/planet.png" class="logo">
+        </div>
+      <section class="navigation">
+      <nav>
+        <ul>
+          <li><a href="table.html">Таблица Менделеева</a> </li>
+        </ul>
+      </nav>
+    </section>
+    </header>
+    <div class="Familia">
+      <h1>Никифоров Никита</h1>
+  </div>
+<section class="Information">
+<div class="Foto">
+  <img class="Myfoto" src="Images/MyFoto.jpeg">
+</div>
+<div class="AboutMe">
+  <?php
+  $str = 'Всем привет! Немного информации обо мне. Работаю реабилитологом! В свободное от работы время занимаюсь дизайном, а также довольно успешно увлекаюсь Stand Upом, выступаю со сцены вот уже 2 года!';
+echo $str;
+  
+?>
+    </div>
+
+  <div class="Otziv">
+    Первые занятия прошли отлично! Понравилось, что в некоторой информации нужно разбираться и искать самому, что лишь побуждает больше погружаться в обучение!
+  </div> 
+</div>
+</section>
+    <p>
+  <section class="container">
+      <section class="Grid1">
+          <section class="GridPic">
+        <div class="picture"> <img src="Images/monkey1.png" alt="monkey1" width="300" height="200"> </div>
+        <div class="description9">NFT Monkey1</div>
+      </section>
+      <section class="GridPic">
+        <div class="picture"> <img src="Images/monkey2.jpg" alt="monkey2" width="300" height="200"> </div>
+        <div class="description9">NFT Monkey2</div>
+      </section>
+      <section class="GridPic">
+        <div class="picture"> <img src="Images/monkey3.jpg" alt="monkey3" width="300" height="200"> </div>
+        <div class="description9">NFT Monkey3</div>
+      </section>
+      <section class="GridPic">
+        <div class="picture"> <img src="Images/monkey4.jpg" alt="monkey4" width="300" height="200"> </div>
+        <div class="description9">NFT Monkey4</div>
+      </section>
+    </section>
+    </p>
+    <p>
+      <section class="Grid2">
+        <section class="GridPic">
+      <div class="picture"> <img src="Images/pic1.jpg" alt="pic1" width="300" height="200"> </div>
+      <div class="description9">В понедельник на работе</div>
+    </section>
+    <section class="GridPic">
+      <div class="picture"> <img src="Images/pic2.jpg" alt="pic2" width="300" height="200"> </div>
+      <div class="description9">Охрана своей кружки в офисе</div>
+    </section>
+    <section class="GridPic">
+      <div class="picture"> <img src="Images/pic3.jpg" alt="pic3" width="300" height="200"> </div>
+      <div class="description9">Скоро пятница</div>
+    </section>
+    <section class="GridPic">
+      <div class="picture"> <img src="Images/pic4.jpg" alt="pic4" width="300" height="200"> </div>
+      <div class="description9">Вечер Пятницы</div>
+    </section>
+  </section>
+    </p>
+</body>
+</html>
